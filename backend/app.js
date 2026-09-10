f4bb13b5d58676c30c2863a94f0fcc44d0a82c09
abdullhghaly <abdullhghaly@gmail.com>
2026-09-10 21:09:22 +0300

    added query in cars.js to car-detail page by id and remove main.html to index.html and change mount api point from / to /api and serve html files

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const carRoutes = require("./routes/cars");
-const customerRoutes = require("./routes/customers");
-const employeeRoutes = require("./routes/employees");
-const saleRoutes = require("./routes/sales");
+const carroutes = require("./routes/cars");
+const customerroutes = require("./routes/customers");
+const employeeroutes = require("./routes/employees");
+const saleroutes = require("./routes/sales");
+const path = require("path");
 
-app.use(express.json()); // to handle JSON requests
+app.use(express.json()); // to handle json requests
 
-app.use("/", carRoutes);
-app.use("/", customerRoutes);
-app.use("/", employeeRoutes);
-app.use("/", saleRoutes);
+app.use("/api", carroutes);
+app.use("/api", customerroutes);
+app.use("/api", employeeroutes);
+app.use("/api", saleroutes);
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, "../frontend/src")));
+
+// Serve static files from frontend/public
+app.use("/public", express.static(path.join(__dirname, "../frontend/public")));
+app.use("/static", express.static(path.join(__dirname, "../frontend/src")));
 
 app.get("/", (req, res) => {
-  res.send("Car Showroom Management System is running!");
+  // res.send("car showroom management system is running!");
+  res.sendFile(path.join(__dirname, "../frontend/src/index.html"));
+});
+
+app.get("/cars", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/src/cars.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`server running at http://localhost:${port}`);
 });
--- backend/routes/cars.js
+++ backend/routes/cars.js
@@ -12,6 +12,21 @@ router.get("/cars", async (req, res) => {
   }
 });
 
+// Get one car by ID
+router.get("/cars/:id", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM cars WHERE id = $1", [
+      req.params.id,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(404).send("Car not found");
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 // Add a car
 router.post("/cars", async (req, res) => {
   const { model, price } = req.body;
